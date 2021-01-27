2972916fc810ea65ae6fc7d7a5ac1d358a5ae4b4
JianYong-Collab <69342160+JianYong-Collab@users.noreply.github.com>
2021-01-27 16:49:51 +0800

    Revert "..."
    This reverts commit 16dacd444a10783770010549aa1ed7076c758fa0.

--- Source1.cpp
+++ Source1.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
+ï»¿#include <iostream>
 
 using namespace std;
 
 int main(void) {
-	std::cout << "Hello World! I’m God and Anime";
-
+	std::cout << "Hello World! Iï¿½m God and Anime";
+	std::cout << "Ookina chungus";
 	std::cout << "Hello World! I'm Depression";
-
-	
+	std::cout << "Hello World! I'm Trash";
+	std::cout << "I have the power the of God and Anime on my side" << std::endl;
 	return 0;
 }
