d150268d6da19ae58a832f7c67a5666f8c405528
IisAndrew <andrewchanjw17@gmail.com>
2021-01-27 16:51:42 +0800

    whee

--- Source1.cpp
+++ Source1.cpp
@@ -8,5 +8,8 @@ int main(void) {
 	std::cout << "Hello World! I'm Depression";
 	std::cout << "Hello World! I'm Trash";
 	std::cout << "I have the power the of God and Anime on my side" << std::endl;
+	int math = 0;
+	math + 3;
+	std::cout << math << std::endl;
 	return 0;
 }
