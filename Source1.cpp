cce1d874a50b403f4aad206beafb628a500c82ae
IisAndrew <andrewchanjw17@gmail.com>
2021-01-27 16:40:06 +0800

    efs

--- Source1.cpp
+++ Source1.cpp
@@ -7,5 +7,6 @@ int main(void) {
 	std::cout << "Ookina chungus";
 	std::cout << "Hello World! I'm Depression";
 	std::cout << "Hello World! I'm Trash";
+	std::cout << "I have the power the of God and Anime on my side" << std::endl;
 	return 0;
 }
